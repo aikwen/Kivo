e4a87e79e49d14d6d2f526e7c660f10d636946f3
aikwen <kwen.zou@outlook.com>
2026-08-23 23:57:46 +0800

    自建 tray

--- src/kivo/tray/main.py
+++ src/kivo/tray/main.py
@@ -1,14 +1,14 @@
 import sys
 
 from PySide6.QtCore import QObject, Signal
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtGui import QCursor, QIcon
 from PySide6.QtWidgets import (
     QApplication,
-    QMenu,
     QSystemTrayIcon,
 )
 
 from kivo.resources.loader import resource_path
+from kivo.tray.menu import TrayMenu
 
 
 class Tray(QObject):
@@ -29,19 +29,11 @@ class Tray(QObject):
 
         self._tray = QSystemTrayIcon(icon, self)
 
-        self._menu = QMenu()
-
-        self._exit_action = QAction(
-            "Exit",
-            self._menu,
-        )
-        self._exit_action.triggered.connect(
+        self._menu = TrayMenu()
+        self._menu.exit_requested.connect(
             self.exit_requested
         )
 
-        self._menu.addAction(self._exit_action)
-
-        self._tray.setContextMenu(self._menu)
         self._tray.activated.connect(
             self._on_activated
         )
@@ -51,6 +43,7 @@ class Tray(QObject):
 
     def hide(self) -> None:
         self._tray.hide()
+        self._menu.hide()
 
     def _on_activated(
         self,
@@ -61,6 +54,55 @@ class Tray(QObject):
             == QSystemTrayIcon.ActivationReason.Trigger
         ):
             self.activated.emit()
+            return
+
+        if (
+            reason
+            == QSystemTrayIcon.ActivationReason.Context
+        ):
+            self._show_menu()
+
+    def _show_menu(self) -> None:
+        cursor = QCursor.pos()
+
+        screen = QApplication.screenAt(cursor)
+        if screen is None:
+            screen = QApplication.primaryScreen()
+
+        if screen is None:
+            return
+
+        geometry = screen.availableGeometry()
+
+        self._menu.adjustSize()
+
+        center = geometry.center()
+        offset = 4
+
+        if cursor.x() < center.x():
+            x = cursor.x() + offset
+        else:
+            x = (
+                cursor.x()
+                - self._menu.width()
+                - offset
+            )
+
+        if cursor.y() < center.y():
+            y = cursor.y() + offset
+        else:
+            y = (
+                cursor.y()
+                - self._menu.height()
+                - offset
+            )
+
+        self._menu.move(x, y)
+
+        self._menu.show()
+        self._menu.raise_()
+        self._menu.activateWindow()
+        self._menu.setFocus()
 
 
 if __name__ == "__main__":
